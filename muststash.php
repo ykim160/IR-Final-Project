ddbae2a852d2c75fcf0a943072cf7039abd897fe
Ye Chan Kim <ykim160@ugradx.cs.jhu.edu>
2018-05-18 18:40:49 -0400

    fixed bug

--- muststash.php
+++ muststash.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
-<link rel="icon" href="mustache.jpg">
+<link rel="icon" href="https://data.whicdn.com/images/85417481/large.jpg">
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
 .w3-bar-block .w3-bar-item {padding:20px}
